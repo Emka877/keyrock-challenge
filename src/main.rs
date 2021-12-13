f26920fefbc5a1c00f452c122205b4489278cc0a
Oscuro87 <capelle.e@gmail.com>
2021-12-13 22:18:57 +0100

    Move test into an actual test file + comment out unused modules for now

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,15 @@ mod tests;
 mod exceptions;
 mod configuration;
 /// This module contains everything related with streaming the final orderbook to the world
-mod grpc_server;
+// mod grpc_server;
 /// Contains everything related to fetching the data from the remote exchanges
 mod exchanges;
 /// This module contains the methods used to merge the final orderbook
-mod orderbook;
+// mod orderbook;
 
 use exchanges::{open_stream_to_exchange, ExchangeEndpoint};
 
 #[tokio::main]
 async fn main() {
-    let mut bitstamp_stream = open_stream_to_exchange(ExchangeEndpoint::Bitstamp).await.expect("Cannot open stream to Bitstamp");
-    let mut binance_stream = open_stream_to_exchange(ExchangeEndpoint::Binance).await.expect("Could open stream to Binance");
     
-    let _ = binance_stream.close(None).await;
-    let _ = bitstamp_stream.close(None).await;
 }
