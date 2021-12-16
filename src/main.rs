5f3a3cc919a2454739b080b0a4a0f1be475274dd
Oscuro87 <capelle.e@gmail.com>
2021-12-16 15:17:46 +0100

    Rewrite main to use latest implementations of streams, subscriptions

--- src/main.rs
+++ src/main.rs
@@ -4,59 +4,38 @@ mod tests;
 mod exceptions;
 mod configuration;
 /// This module contains everything related with streaming the final orderbook to the world
-// mod grpc_server;
+mod grpc_server;
 /// Contains everything related to fetching the data from the remote exchanges
 mod exchanges;
 /// This module contains the methods used to merge the final orderbook
-// mod orderbook;
+mod orderbook;
 
 use exchanges::{open_stream_to_exchange, ExchangeEndpoint};
-use tungstenite::Message;
-use futures::{StreamExt, SinkExt};
+use futures::StreamExt;
 
-use crate::exchanges::Bitstamp;
+use crate::exchanges::{Binance, BinanceData, Bitstamp, BitstampData, Exchange, ExchangeWsTcpStream};
 
 #[tokio::main]
 async fn main() {
-    // TODO: Move all this code as an actual test
-    let mut bitstamp = open_stream_to_exchange(ExchangeEndpoint::Bitstamp).await.expect("Error opening stream to Bitstamp.");
-    // let mut binance = open_stream_to_exchange(ExchangeEndpoint::Binance).await.expect("Cannot open stream to Binance.");
-    
-    let bitstamp_subscribe_message = bitstamp.send(Message::Text(r#"{
-        "event": "bts:subscribe",
-        "data": {
-            "channel": "order_book_ethbtc"
-        }
-    }"#.to_owned())).await;
-    
-    if bitstamp_subscribe_message.is_err() {
-        eprintln!("Could not subscribe to ethbtc.");
-    } else {
-        let _ = bitstamp.next().await;
-    }
+    let mut bitstamp = open_stream_to_exchange(ExchangeEndpoint::Bitstamp).await.expect("Cannot open stream to Bitstamp.");
+    let _ = Bitstamp::subscribe_to_orderbook_stream(&mut bitstamp).await;
+    let mut binance = open_stream_to_exchange(ExchangeEndpoint::Binance).await.expect("Cannot open stream to Binance.");
+    let _ = Binance::subscribe_to_orderbook_stream(&mut binance).await;
 
+    // TODO: !!Error handling in stream reading.
     loop {
-        tokio::select! {
-            bitstamp_msg = bitstamp.next() => {
-                match bitstamp_msg {
-                    Some(msg) => match msg {
-                        Ok(msg) => match msg {
-                            Message::Binary(x) => println!("binary {:?}", x),
-                            Message::Text(data) => {
-                                let incoming: Bitstamp = serde_json::from_str(&data).unwrap();
-                                println!("{:?}", incoming);
-                            },
-                            Message::Ping(x) => {},
-                            Message::Pong(x) => {},
-                            Message::Close(reason) => { println!("Connection closed: {:?}", reason); },
-                        },
-                        Err(_) => {println!("server went away"); break;}
-                        },
-                    None => {println!("no message"); break;},
-                }
-            }
-        };
-    }
+        let bitstamp_message = bitstamp.next().await;
+        if bitstamp_message.is_some() && bitstamp_message.as_ref().unwrap().is_ok() {
+            // Shadowing previous var
+            let bitstamp_message = bitstamp_message.unwrap().unwrap();
+            let bitstamp_data: BitstampData = serde_json::from_str(bitstamp_message.to_string().as_str()).unwrap();
+        }
 
-    let _ = bitstamp.send(Message::Close(None)).await;
+        let binance_message = binance.next().await;
+        if binance_message.is_some() && binance_message.as_ref().unwrap().is_ok() {
+            let binance_message = binance_message.unwrap().unwrap();
+            let binance_data: BinanceData = serde_json::from_str(binance_message.to_text().unwrap()).unwrap();
+            println!("{:?}", binance_data);
+        }
+    }
 }
