425e27b6c1708ff1551811ec4db5e1a5e0a3247c
Oscuro87 <capelle.e@gmail.com>
2021-12-21 17:23:34 +0100

    Give it a type so it builds

--- src/grpc/server/server.rs
+++ src/grpc/server/server.rs
@@ -1,5 +1,6 @@
+use std::pin::Pin;
 use tonic::{Request, Response, Status};
-use crate::grpc::server::orderbook::Empty;
+use crate::grpc::server::orderbook::{Empty, Summary};
 use crate::grpc::server::orderbook::orderbook_aggregator_server::OrderbookAggregator;
 use tokio_stream::{Stream, StreamExt};
 
@@ -9,7 +10,8 @@ pub struct OrderbookServer {
 
 #[tonic::async_trait]
 impl OrderbookAggregator for OrderbookServer {
-    type BookSummaryStream = ();
+    type BookSummaryStream =
+        Pin<Box<dyn Stream<Item = Result<Summary, Status>> + Send + Sync + 'static>>;
 
     async fn book_summary(&self, request: Request<Empty>) -> Result<Response<Self::BookSummaryStream>, Status> {
         todo!()
