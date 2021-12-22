2d2e1d2656b75a6b587df0d20439adc1f5c0159b
Oscuro87 <capelle.e@gmail.com>
2021-12-22 17:40:09 +0100

    Update store.rs

--- src/persistence/store.rs
+++ src/persistence/store.rs
@@ -7,15 +7,18 @@ lazy_static!(
 );
 
 fn initialize_store() -> RwLock<LocalSummary> {
+    // dbg!("The orderbook is being initialized.");
     let initial_orderbook: LocalSummary = LocalSummary::new(&APP_CONFIG.currency_pair.clone());
     RwLock::new(initial_orderbook)
 }
 
 pub fn update_merged_orderbook(new_orderbook: LocalSummary) {
     *LATEST_ORDERBOOK.write().unwrap() = new_orderbook;
+    // dbg!("The orderbook was updated!");
 }
 
 pub fn get_merged_orderbook() -> LocalSummary {
+    // dbg!("The orderbook is being accessed.");
     let original = LATEST_ORDERBOOK.read().unwrap();
     original.clone()
 }
