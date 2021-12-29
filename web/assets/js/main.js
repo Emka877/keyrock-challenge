47e0c6a3bad76628c73606b61c455d5f4bd535e8
Oscuro87 <capelle.e@gmail.com>
2021-12-29 01:49:45 +0100

    Add web server

---
+++ web/assets/js/main.js
@@ -0,0 +1,78 @@
+console.log("main.js loaded.");
+
+// let queryDataButton = document.getElementById("btn-query-data");
+let asksData = document.getElementById("asks-data");
+let bidsData = document.getElementById("bids-data");
+let spreadData = document.getElementById("spread-data");
+
+const QUERY_INTERVAL_MS = 200;
+let query_timeout_object = null;
+
+function initialize() {
+    // queryDataButton.onclick = ((evt) => {
+    //     evt.preventDefault();
+    //     getQuotesFromServer();
+    // });
+
+    getQuotesFromServer();
+    query_timeout_object = setInterval(() => getQuotesFromServer(), QUERY_INTERVAL_MS);
+}
+
+function quoteObjectToDOM(quote) {
+    let element = document.createElement("div");
+    
+    // Sub elements
+    // Alternate method: Iterate over object keys and create a <span> for each key, then put value in innerHTML
+    let exchange = document.createElement("div");
+    exchange.className = "exchange";
+    exchange.innerHTML = quote.exchange;
+    let price = document.createElement("div");
+    price.className = "price";
+    price.innerHTML = quote.price;
+    let amount = document.createElement("div");
+    amount.className = "amount";
+    amount.innerHTML = quote.amount;
+    
+    // Merge it all
+    element.appendChild(exchange);
+    element.appendChild(price);
+    element.appendChild(amount);
+    
+    return element;
+}
+
+function getQuotesFromServer() {
+    fetch("http://127.0.0.1:3000/get_quotes")
+        .then(data => data.json())
+        .then(res => {
+            if (res.noDataCached) {
+                console.error("Response received from server, but data is empty!");
+                return;
+            }
+            
+            // console.log(res.data);
+            
+            // asksData.innerHTML = JSON.stringify(res.data.asks);
+            // bidsData.innerHTML = JSON.stringify(res.data.bids);
+            asksData.innerHTML = "";
+            res.data.asks.forEach(quote => {
+                let dom = quoteObjectToDOM(quote).innerHTML;
+                asksData.innerHTML += "<div class='quotes-data-root'>" + dom + "</div>";
+            });
+
+            bidsData.innerHTML = "";
+            res.data.bids.forEach(quote => {
+                let dom = quoteObjectToDOM(quote).innerHTML;
+                bidsData.innerHTML += "<div class='quotes-data-root'>" + dom + "</div>";
+            });
+
+            spreadData.innerHTML = res.data.spread;
+        })
+        .catch(err => console.error(err));
+}
+
+function main() {
+    initialize();
+}
+
+main();
---
+++ web/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const grpc = require("@grpc/grpc-js");
+const grpcProtoLoader = require("@grpc/proto-loader");
+const path = require("path");
+const cors = require("cors");
+const { assert } = require("console");
+const app = express();
+const _ = require("lodash");
+const { cp } = require("fs");
+
+const PORT = 3000;
+const REMOTE_ENDPOINT = "keyrock.oscur.io:50005";
+const REMOTE_REQUEST_URI = "/orderbook.OrderbookAggregator/BookSummary";
+
+/** GRPC client **/
+let cache = {};
+let packageDefinition = grpcProtoLoader.loadSync(
+    path.join(__dirname, "assets/prototype/schema.proto"),
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+    }
+);
+let routeGuide = grpc.loadPackageDefinition(packageDefinition).orderbook;
+let client = new routeGuide.OrderbookAggregator(REMOTE_ENDPOINT, grpc.credentials.createInsecure());
+// console.log(client);
+let stream = client.BookSummary();
+
+const readStream = (stream) => {
+    stream.on('data', (data) => {
+        cache = data;
+        return;
+    });
+};
+readStream(stream);
+
+
+/** Web server **/
+// Don't do this at home ↓
+app.use(cors({ origin: "*" }));
+app.use("/static", express.static(path.join(__dirname, "assets")));
+
+app.get("/", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/assets/html/index.html"));
+});
+
+app.get("/get_quotes", (req, res, next) => {
+    // console.log("Asking for cache...");
+    // console.log(cache);
+    res.send({ data: cache });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://127.0.0.1:${PORT}`);
+});
